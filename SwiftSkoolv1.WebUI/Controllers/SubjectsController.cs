e178991eabb40b218f7973a5cb14f06eb1f6a95f
AJILEYE AYOKUNLE J <kunlesymls@hotmail.com>
2017-07-15 20:46:47 +0100

    Making UI responsive

--- SwiftSkoolv1.WebUI/Controllers/SubjectsController.cs
+++ SwiftSkoolv1.WebUI/Controllers/SubjectsController.cs
@@ -40,7 +40,8 @@ namespace SwiftSkoolv1.WebUI.Controllers
             int skip = start != null ? Convert.ToInt32(start) : 0;
             int totalRecords = 0;
 
-            var v = Db.Subjects.Where(x => x.SchoolId != userSchool).Select(s => new { s.SubjectId, s.SubjectCode, s.SubjectName }).ToList();
+            //var v = Db.Subjects.Where(x => x.SchoolId != userSchool).Select(s => new { s.SubjectId, s.SubjectCode, s.SubjectName }).ToList();
+            var v = Db.Subjects.Where(x => x.SchoolId == userSchool).Select(s => new { s.SubjectId, s.SubjectCode, s.SubjectName }).ToList();
 
             //var v = Db.Subjects.Where(x => x.SchoolId.Equals(userSchool)).Select(s => new { s.SubjectId, s.SubjectCode, s.SubjectName }).ToList();
             //if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortColumnDir)))
