3a2765fff0f8722b7a48d1d0572180c91422c6e8
AJILEYE AYOKUNLE J <kunlesymls@hotmail.com>
2017-07-18 12:16:26 +0100

    Changing UI and adding js

--- SwiftSkoolv1.WebUI/ViewModels/StudentDashboardVm.cs
+++ SwiftSkoolv1.WebUI/ViewModels/StudentDashboardVm.cs
@@ -1,5 +1,6 @@
-﻿using System.Collections.Generic;
-using SwiftSkoolv1.Domain;
+﻿using SwiftSkoolv1.Domain;
+using System.Collections.Generic;
+using SwiftSkool.Services;
 
 namespace SwiftSkoolv1.WebUI.ViewModels
 {
@@ -22,15 +23,24 @@ namespace SwiftSkoolv1.WebUI.ViewModels
         public List<AssignSubjectTeacher> SubjectTeachers { get; set; }
     }
 
+
+
     public class AdminDashboardVm
     {
         public string ClassName { get; set; }
-        public List<string> Subjects { get; set; }
+        public int SubjectCount { get; set; }
         public int MaleStudents { get; set; }
         public int FemaleStudents { get; set; }
+        public double MalePercentage { get; set; }
+        public double FemalePercentage { get; set; }
         public int TotalStudents { get; set; }
-        public string FormTeacher { get; set; }
-        public string Term { get; set; }
-        public string Session { get; set; }
+        public string CurrentTerm { get; set; }
+        public string CurrentSession { get; set; }
+        public int TotalNumberOfStaff { get; set; }
+        public int ActiveStudent { get; set; }
+        public int GraduatedStudent { get; set; }
+
+        public List<DataPoint> DataPoint { get; set; }
+
     }
 }
